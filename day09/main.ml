2c9dffbd277b49f943626e3f3d3f5617472e8015
Fabian Carlström <inrick@gmail.com>
2017-12-09 12:04:35 +0100

    Keep track of current groups value instead
    Maps more clearly to problem description and avoids an extra addition.

--- day09/main.ml
+++ day09/main.ml
@@ -5,17 +5,17 @@ type state = Regular | Garbage
 
 let () =
   let t = List.(In_channel.read_lines "input" >>= String.to_list) in
-  let rec go prev sum nc state stream =
+  let rec go curr sum nc state stream =
     match state, stream with
     | _, [] -> sum, nc
-    | Regular, '<'::xs -> go prev sum nc Garbage xs
-    | Regular, '{'::xs -> go (prev+1) (sum+prev+1) nc Regular xs
-    | Regular, '}'::xs -> go (prev-1) sum nc Regular xs
-    | Regular, _::xs -> go prev sum nc Regular xs
-    | Garbage, '>'::xs -> go prev sum nc Regular xs
-    | Garbage, '!'::_::xs -> go prev sum nc Garbage xs
-    | Garbage, _::xs -> go prev sum (nc+1) Garbage xs
+    | Regular, '<'::xs -> go curr sum nc Garbage xs
+    | Regular, '{'::xs -> go (curr+1) (sum+curr) nc Regular xs
+    | Regular, '}'::xs -> go (curr-1) sum nc Regular xs
+    | Regular, _::xs -> go curr sum nc Regular xs
+    | Garbage, '>'::xs -> go curr sum nc Regular xs
+    | Garbage, '!'::_::xs -> go curr sum nc Garbage xs
+    | Garbage, _::xs -> go curr sum (nc+1) Garbage xs
   in
-  let a, b = go 0 0 0 Regular t in
+  let a, b = go 1 0 0 Regular t in
   printf "a) %d\n" a;
   printf "b) %d\n" b
